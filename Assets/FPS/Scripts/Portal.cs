3f28c8b3c2593690a6caed3cb08dbb53897dc2c0
Dmitry <divgorbunov@gmail.com>
2020-04-09 01:29:52 +0300

    Initial commit with data

---
+++ Assets/FPS/Scripts/Portal.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private static GameObject spawnedBluePortal = null;
+    private static GameObject spawnedRedPortal = null;
+
+    public bool isBlue = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isBlue)
+        {
+            if (spawnedBluePortal)
+            {
+                Destroy(spawnedBluePortal);
+            }
+            spawnedBluePortal = this.gameObject;
+            if (spawnedRedPortal)
+            {
+                Camera cam = spawnedRedPortal.GetComponentInChildren<Camera>();
+                cam.transform.position = this.transform.position;
+                cam.transform.forward = this.transform.up;
+
+                cam = this.gameObject.GetComponentInChildren<Camera>();
+
+                cam.transform.position = spawnedRedPortal.transform.position;
+                cam.transform.forward = spawnedRedPortal.transform.up;
+            }
+        }
+        else {
+            if (spawnedRedPortal)
+            {
+                Destroy(spawnedRedPortal);
+            }
+            spawnedRedPortal = this.gameObject;
+            if (spawnedBluePortal)
+            {
+                Camera cam = spawnedBluePortal.GetComponentInChildren<Camera>();
+                cam.transform.position = this.transform.position;
+                cam.transform.forward = this.transform.up;
+                cam = this.gameObject.GetComponentInChildren<Camera>();
+
+                cam.transform.position = spawnedBluePortal.transform.position;
+                cam.transform.forward = spawnedBluePortal.transform.up;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider)
+        {
+            if (spawnedBluePortal && spawnedBluePortal)
+            {
+                Vector3 telep;
+                Vector3 upVector;
+                if (isBlue)
+                {
+                    telep = spawnedRedPortal.transform.position;
+                    upVector = spawnedRedPortal.transform.up;
+                    telep += upVector * 0.5f;
+                }
+                else {
+                    telep = spawnedBluePortal.transform.position;
+                    upVector = spawnedBluePortal.transform.up;
+                    telep += upVector * 0.5f;
+                }
+
+                PlayerCharacterController controller = collider.gameObject.GetComponent<PlayerCharacterController>();
+
+                if (controller)
+                {
+                    controller.teleportTo(telep, upVector);
+                }
+                else
+                {
+                    ProjectileStandard projectile = collider.gameObject.GetComponent<ProjectileStandard>();
+                    if (projectile)
+                    {
+                        projectile.teleportTo(telep, upVector);
+                    }
+                }
+            }
+                     
+        }
+    }
+}
---
+++ Assets/FPS/Scripts/PortalProjectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalProjectile : ProjectileStandard
+{
+    public GameObject PortalPrefab;
+    override public void OnHit(Vector3 point, Vector3 normal, Collider collider)
+    {
+        //Quaternion quat = new Quaternion(normal.x, normal.y, normal.z, 1.0f);
+
+        GameObject portal = GameObject.Instantiate<GameObject>(PortalPrefab);
+        Transform portalTransform = portal.GetComponent<Transform>();
+        portalTransform.position = point;
+        portalTransform.up = normal;
+
+        // impact vfx
+        if (impactVFX)
+        {
+            GameObject impactVFXInstance = Instantiate(impactVFX, point + (normal * impactVFXSpawnOffset), Quaternion.LookRotation(normal));
+            if (impactVFXLifetime > 0)
+            {
+                Destroy(impactVFXInstance.gameObject, impactVFXLifetime);
+            }
+        }
+
+        // impact sfx
+        if (impactSFXClip)
+        {
+            AudioUtility.CreateSFX(impactSFXClip, point, AudioUtility.AudioGroups.Impact, 1f, 3f);
+        }
+
+        // Self Destruct
+        Destroy(this.gameObject);
+    }
+}
